2a10bfb2b611edb725858b439d819c2e56183beb
Andrez <andrez.quix@gmail.com>
2024-05-15 01:25:48 +0300

    Add Fibonacci

---
+++ 4.AAlgorithms/Fibonacci.cs
@@ -0,0 +1,34 @@
+﻿using Testing;
+
+namespace FiboAlg
+{
+    public class Fibonacci : ITask
+    {
+        public string Run(string[] data)
+        {
+            double a = Convert.ToInt32(data[0]);
+            double res = FibonacciIter(a);
+            return res.ToString();
+        }
+
+        public static double FibonacciRec(double a)
+        {
+            if (a == 0 || a == 1) return a;
+            return FibonacciRec(a - 1) + FibonacciRec(a - 2);
+        }
+
+        public static double FibonacciIter(double a)
+        {
+            double f0 = 0;
+            double f1 = 1;
+            double f2 = 0;
+            for(int i = 2; i < a; i++)
+            {
+                f2 = f0 + f1;
+                f0 = f1;
+                f1 = f2;
+            }
+            return f2;
+        }
+    }
+}
