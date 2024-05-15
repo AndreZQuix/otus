cbf5145b56db0c1cbad3fcb7116fa6436db8f7a9
Andrez <andrez.quix@gmail.com>
2024-05-15 17:36:35 +0300

    Add matrix

---
+++ 4.AAlgorithms/Matrix.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.AAlgorithms
+{
+    public class Matrix
+    {
+        private readonly double[,] data;
+        public int Height => data.GetUpperBound(0) + 1;
+        public int Width => data.GetUpperBound(1) + 1;
+
+        public ref double this[int row, int column] => ref data[row, column];
+
+        public Matrix(int h, int w, double d = 1.0)
+        {
+            data = new double[h, w];
+            for(int i = 0; i < h; i++)
+            {
+                for(int j = 0; j < w; j++)
+                {
+                    data[i, j] = d;
+                }
+            }
+        }
+
+        public Matrix(double[,] data)
+        {
+            this.data = data;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Console.Write($"{data[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public bool IsMultiplyPossible(Matrix matrix)
+        {
+            return Width == matrix.Height;
+        }
+
+        public Matrix Multiply(Matrix matrix)
+        {
+            if(!IsMultiplyPossible(matrix))
+            {
+                Console.WriteLine("Unable to multiply");
+                return new Matrix(Height, Width, -1.0);
+            }
+
+            Matrix result = new Matrix(Height, matrix.Width, 0);
+            for(int i = 0; i < Height; i++)
+            {
+                for(int j = 0; j < matrix.Width; j++)
+                {
+                    for(int k = 0; k < matrix.Height; k++)
+                    {
+                        result[i,j] += this[i, k] * matrix[k, j];
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
