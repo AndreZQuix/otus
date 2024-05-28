8a37c8d81344fc09729dfc50efc886f69b214dcf
Andrez <andrez.quix@gmail.com>
2024-05-29 00:10:56 +0300

    Add GetOnesBit

--- BitManipulation/Program.cs
+++ BitManipulation/Program.cs
@@ -2,10 +2,41 @@
 
 BitBoard king = new King("11");
 Console.Clear();
-king.Print();
 king.Steps();
-king.Print(11);
+king.Print();
 
 BitBoard knight = new Knight("27");
 knight.Steps();
-knight.Print(22);
+knight.Print(11);
+
+Console.SetCursorPosition(0, 22);
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+
+static int GetOnesBit_rev1(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        if ((num & 0x01) > 0)
+        {
+            count++;
+        }
+        num >>= 1;
+    }
+    return count;
+}
+
+static int GetOnesBit_rev2(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        count++;
+        num &= (num - 1);
+    }
+    return count;
+}
+
+Console.WriteLine(GetOnesBit_rev1(255));
+Console.WriteLine(GetOnesBit_rev2(255));
