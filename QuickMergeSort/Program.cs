226a7122e1920eb3e1f2d5e36ea81f24bef6608f
Andrez <andrez.quix@gmail.com>
2024-05-30 18:37:39 +0300

    Add quick sort

---
+++ QuickMergeSort/Program.cs
@@ -0,0 +1,16 @@
+﻿using QuickMergeSort;
+
+
+int[] arr = { 51, 95, 66, 72, 42, 38, 39, 41, 15 };
+int N = arr.Length;
+Console.WriteLine("Unsorted:");
+for (int i = 0; i < N; i++)
+    Console.Write(arr[i] + " ");
+Console.WriteLine();
+
+QuickSort srt = new QuickSort();
+srt.QuickSortAlg(ref arr, 0, N - 1);
+Console.WriteLine("Quick sort:");
+for (int i = 0; i < N; i++)
+    Console.Write(arr[i] + " ");
+Console.WriteLine();
---
+++ QuickMergeSort/QuickSort.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace QuickMergeSort
+{
+    public class QuickSort
+    {
+        private void Swap(ref int[] arr, int index1, int index2)
+        {
+            int temp = arr[index1];
+            arr[index1] = arr[index2];
+            arr[index2] = temp;
+        }
+
+        private int Partition(ref int[] array, int low, int high)
+        {
+            int pivot = array[(low + high) / 2];
+            int i = low - 1;
+            int j = high + 1;
+            while (true)
+            {
+                do
+                {
+                    i++;
+                } while (array[i] < pivot);
+
+                do
+                {
+                    j--;
+                } while (array[j] > pivot);
+
+                if (i >= j)
+                    return j;
+
+                Swap(ref array, i, j);
+            }
+        }
+
+        public void QuickSortAlg(ref int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivotIndex = Partition(ref array, low, high);
+                QuickSortAlg(ref array, low, pivotIndex - 1);
+                QuickSortAlg(ref array, pivotIndex + 1, high);
+            }
+        }
+    }
+}
