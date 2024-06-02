dd993a84b12722b619e92f9703e1791f2649e304
Andrez <andrez.quix@gmail.com>
2024-06-02 14:04:23 +0300

    Add selection sort

---
+++ SelectionHeapSort/Program.cs
@@ -0,0 +1,23 @@
+﻿using SelectionHeapSort;
+using System.Diagnostics;
+
+int[] CreateRandomArray(int length)
+{
+    int[] array = new int[length];
+    Random random = new Random();
+    for(int i = 0; i < length; i++)
+    {
+        array[i] = random.Next(0, 100);
+    }
+    return array;
+}
+
+for (int i = 100; i <= 1000000; i *= 10)
+{
+    int[] arr = CreateRandomArray(i);
+    SelectionSort srt = new();
+    Stopwatch sw = Stopwatch.StartNew();
+    srt.SelectionSortAlg(ref arr);
+    sw.Stop();
+    Console.WriteLine($"Elapsed selection sort time for {i} elements: {sw.ElapsedMilliseconds} ms");
+}
---
+++ SelectionHeapSort/SelectionSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionHeapSort
+{
+    public class SelectionSort : ITask
+    {
+        private void Swap(ref int[] arr, int index1, int index2)
+        {
+            int temp = arr[index1];
+            arr[index1] = arr[index2];
+            arr[index2] = temp;
+        }
+
+        public void SelectionSortAlg(ref int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; ++i)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < arr.Length; ++j)
+                {
+                    if (arr[j] < arr[minIndex])
+                        minIndex = j;
+                }
+                Swap(ref arr, minIndex, i);
+            }
+        }
+    }
+}
