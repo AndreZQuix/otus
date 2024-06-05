7cea4c572a07849ec8be71a0db17a59046b551bc
Andrez <andrez.quix@gmail.com>
2024-06-05 18:05:16 +0300

    Add bucket sort

---
+++ LinearSort/BuckerSort.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSort
+{
+    public class BuckerSort
+    {
+        public int GetMax(int[] arr, int knownMax = -1)
+        {
+            if (knownMax >= 0)  // если максимальное число уже известно
+                return knownMax;
+
+            int max = arr[0];
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+            return max;
+        }
+
+        public void Sort(ref int[] arr, int knownMax = -1)
+        {
+            const int bucketsCount = 10;
+            IList<int>[] buckets = new List<int>[bucketsCount];
+
+            int max = GetMax(arr, knownMax);
+            int j;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int m = (arr[i] / (max + 1)) * bucketsCount;
+                //int m = arr[i] * bucketsCount / 1000;
+                if (buckets[m] == null)
+                {
+                    buckets[m] = new List<int>();
+                }
+
+                if (buckets[m].Count == 0)
+                {
+                    buckets[m].Add(arr[i]);
+                }
+                else
+                {
+                    j = 0;
+                    while((j < buckets[m].Count) && (buckets[m][j] < arr[i]))
+                    {
+                        j++;
+                    }
+                    buckets[m].Insert(j, arr[i]);
+                }
+            }
+
+            j = 0;
+            for(int i = 0; i < buckets.Length; i++)
+            {
+                if (buckets[i] == null)
+                    continue;
+
+                for(int k = 0; k < buckets[i].Count; k++)
+                {
+                    arr[j++] = buckets[i][k];
+                }
+            }
+        }
+    }
+}
---
+++ LinearSort/Program.cs
@@ -0,0 +1,23 @@
+﻿using LinearSort;
+using System.Diagnostics;
+
+int[] CreateRandomArray(int length)
+{
+    int[] array = new int[length];
+    Random random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = random.Next(0, 999);
+    }
+    return array;
+}
+
+for (int i = 100; i <= 1000000; i *= 10)
+{
+    int[] arr = CreateRandomArray(i);
+    BuckerSort srt = new();
+    Stopwatch sw = Stopwatch.StartNew();
+    srt.Sort(ref arr, 999);
+    sw.Stop();
+    Console.WriteLine($"Elapsed bucket sort time for {i} elements: {sw.ElapsedMilliseconds} ms");
+}
