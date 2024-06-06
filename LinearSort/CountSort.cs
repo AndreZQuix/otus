a81369612f881fba7663567a777d2bbc678e7c76
Andrez <andrez.quix@gmail.com>
2024-06-06 19:26:29 +0300

    Add count sort

---
+++ LinearSort/CountSort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSort
+{
+    public class CountSort
+    {
+        public int GetMax(int[] arr)
+        {
+            int max = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+            return max;
+        }
+
+        public void Sort(ref int[] arr)
+        {
+            int max = GetMax(arr);
+            int[] T = new int[max + 1];
+            int[] Z = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                T[arr[i]]++;
+            }
+
+            int z = 0;
+            for (int i = 0; i <= max; i++)
+            {
+                z += T[i];
+                T[i] = z;
+            }
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                T[arr[i]]--;
+                Z[T[arr[i]]] = arr[i];
+            }
+
+            for(int i = 0; i < arr.Length; ++i)
+            {
+                arr[i] = Z[i];
+            }
+        }
+    }
+}
--- LinearSort/Program.cs
+++ LinearSort/Program.cs
@@ -21,3 +21,22 @@ for (int i = 100; i <= 1000000; i *= 10)
     sw.Stop();
     Console.WriteLine($"Elapsed bucket sort time for {i} elements: {sw.ElapsedMilliseconds} ms");
 }
+
+//int[] arr = [0, 1, 1, 1, 2, 0, 1, 1, 0, 2];
+//CountSort srt = new();
+//srt.Sort(ref arr);
+//for(int i = 0; i < arr.Length; i++)
+//{
+//    Console.Write(arr[i] + " ");
+//}
+//Console.WriteLine();
+
+for (int i = 100; i <= 1000000; i *= 10)
+{
+    int[] arr = CreateRandomArray(i);
+    CountSort srt = new();
+    Stopwatch sw = Stopwatch.StartNew();
+    srt.Sort(ref arr);
+    sw.Stop();
+    Console.WriteLine($"Elapsed count sort time for {i} elements: {sw.ElapsedMilliseconds} ms");
+}
